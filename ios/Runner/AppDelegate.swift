6a73446a230c7075a00b7a3ccbe9f7212af9ab9f
narzu11ayevnodirbek <nodirbeknarzullayev2004@gmail.com>
2025-06-25 19:29:41 +0500

    ios native code updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,17 +2,6 @@ import Flutter
 import UIKit
 import AVFoundation
 
-// @main
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
-
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   private let channelName = "com.example.flashlight/torch"
